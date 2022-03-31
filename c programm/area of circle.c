48f48462ac263a5201d86499114aa1540d080290
fulendrabhagat <bhagatrahul984@gmail.com>
2022-03-31 19:56:50 +0530

    area of cicle

---
+++ c programm/area of circle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+ 
+ int main()
+ {
+ 	float pie=3.14;
+ 	int r;
+ 	printf("enter the radius");
+ 	scanf("%d",&r);
+ 	printf("area of circle=%.3f",pie*r*r);
+ }
+ 	
+ 	
+ 

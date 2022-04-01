f5af30926154237af5c48db1256cf4b5e2c458ca
fulendrabhagat <bhagatrahul984@gmail.com>
2022-04-01 12:33:52 +0530

    density

---
+++ c programm/density.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   //forumla of Density =mass/volume
+   
+   int m,v,density;
+   printf("ENTER THE MASS AND VOLUME : ");
+   scanf("%d%d",&m,&v);
+   density=m/v;
+   printf("density = %d",density);
+}

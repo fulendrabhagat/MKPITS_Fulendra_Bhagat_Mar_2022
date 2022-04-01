1abeb4b212b72bb31bad02988936ee1cb4f00c30
fulendrabhagat <bhagatrahul984@gmail.com>
2022-04-01 12:36:21 +0530

    formula of kinetic energy

---
+++ c programm/kinetic energy.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	// kinetic energy  e=12*m*v*v
+	 int m,v,e;
+	 printf("ENTER THE MASS AND VELOCITY : ");
+	 scanf("%d%d",&m,&v);
+	 e=12*m*v*v;
+	 printf("KINETIC ENERGY = %d",e);
+	 
+}

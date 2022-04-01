89491611bb9bbcfebfc5ae98869f092de7672d36
fulendrabhagat <bhagatrahul984@gmail.com>
2022-04-01 16:59:18 +0530

    algorithm

---
+++ c programm/algorithm.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	
+	int a,b,c;
+	printf("enter the value of a,b and c");
+	scanf("%d%d%d",&a,&b,&c);
+	printf("\nsum=%d",a+b+c);
+	printf("\nmult=%d",a*b);
+	printf("\nsub=%d",a-b-c);
+	printf("\ndiv=%d",a/b/c);
+	printf("\nrim=%d",a%b);
+}

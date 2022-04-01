cf404d8833320e190f4eda57ffe9a55c5ae9adb5
fulendrabhagat <bhagatrahul984@gmail.com>
2022-04-01 11:36:10 +0530

    volume of cube

---
+++ c programm/cube.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	
+	int a,volume;
+	printf("ENTER THE VALUE :");
+	scanf("%d",&a);
+	volume=a*a*a;     //formula of cube
+	printf("THE VOLUME OF CUBE=%d",volume);
+	
+}

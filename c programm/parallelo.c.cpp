cadeebe38ba0b1f3af911c68d94e162d2c38b534
fulendrabhagat <bhagatrahul984@gmail.com>
2022-04-01 11:41:03 +0530

    parrallelogram

---
+++ c programm/parallelo.c.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	// perimeter of parallalogram =2*(a+b)
+	
+	int a,b,p;
+	printf("enter the value of a and b  :");
+	scanf("%d%d",&a,&b);
+	p=2*(a+b);
+	printf("perimeter of parallelogram=%d",p);
+}

584e9c0bc611ae5839676d0c34c2f5fc8bc061f0
fulendrabhagat <bhagatrahul984@gmail.com>
2022-04-01 13:42:22 +0530

    speed formula

---
+++ c programm/speed.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	// speed formula 
+	// speed=distance/time
+	
+	int d,t,s;
+	printf("Enter the Distance and Time : ");
+	scanf("%d%d",&d,&t);
+	s=d/t;
+	printf("speed=%d",s);
+	
+}

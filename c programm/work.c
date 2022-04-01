56b0d1e3fe60998cb0e8c3af62a21083076a37d9
fulendrabhagat <bhagatrahul984@gmail.com>
2022-04-01 12:31:47 +0530

    formula of work

---
+++ c programm/work.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	// formula of work=force * displacement
+	int f,s,work;
+	printf("ENTER THE FORCE AND DISPLACEMENT  :");
+	scanf("%d%d",&f,&s);
+	work=f*s;
+	printf("work = %d",work);
+	
+	
+}

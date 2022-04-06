11f32dcf3a87e790bf32cfc09a7ecdb82705bab1
fulendrabhagat <bhagatrahul984@gmail.com>
2022-04-06 13:38:44 +0530

    area of rect and perimeter

---
+++ c programm/rectq7.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int h=7 ,w=5,perimtr,area;
+	perimtr=2*(h+w);
+	area=h*w;
+	
+	printf("   perimeter of rectangle=%d",perimtr);
+	printf("    \n   area of rectangle=%d",area);
+}

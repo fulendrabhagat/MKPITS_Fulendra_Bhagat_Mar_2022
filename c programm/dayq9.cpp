7d12e01dda02abe14754f127ae39a7fd6346d7c1
fulendrabhagat <bhagatrahul984@gmail.com>
2022-04-06 13:44:42 +0530

    day,week,year

---
+++ c programm/dayq9.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int days,years,weeks;
+	printf("enter days :");
+	scanf("%d",&days);
+	years=(days/365);
+	weeks=(days%365)/7;
+	days=days-((years*365)+(weeks*7));
+	printf("YEARS : %d\n",years);
+	printf("WEEKS : %d\n",weeks);
+	printf("DAYS : %d\n",days);
+}

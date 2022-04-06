f226510b0d869e2442ef2302edca21aaae5c1362
fulendrabhagat <bhagatrahul984@gmail.com>
2022-04-06 13:34:14 +0530

    write fandc

---
+++ c programm/fandc.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("   ######\n");
+	printf("   #\n");
+	printf("   #\n");
+	printf("   #####\n");
+	printf("   #\n");
+	printf("   #\n");
+	printf("   #\n\n\n");
+	
+	
+	printf("    ######\n");
+	printf("  ##      ##\n");
+	printf(" #\n");
+	printf(" #\n");
+	printf(" #\n");
+	printf(" #\n");
+	printf(" #\n");
+	printf("  ##      ##\n");
+	printf("    ######\n");
+}

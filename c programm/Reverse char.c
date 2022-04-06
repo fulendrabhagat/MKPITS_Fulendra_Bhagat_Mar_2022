8515ba14918d16fe4a604a3e5bd5003da332f609
fulendrabhagat <bhagatrahul984@gmail.com>
2022-04-06 13:36:12 +0530

    Reverse character

---
+++ c programm/Reverse char.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char a='X';
+	char b='M';
+	char c='L';
+//	printf("Enter any three character ");
+//	scanf("%c%c%c",&a,&b,&c);
+	
+	printf("The reverse of %c%c%c is %c%c%c",a,b,c,c,b,a);
+}

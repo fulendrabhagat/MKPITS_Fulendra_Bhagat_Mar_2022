9225360c954e47f6aac538f9fd587baddedb9bbb
fulendrabhagat <bhagatrahul984@gmail.com>
2022-04-12 10:00:48 +0530

    find quadrant

---
+++ c programm/FinDQUADRANT Q33.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int x,y;
+	printf("INPUT CO-ORDINATE X,Y : ");
+	
+	printf("\nenter x :");
+	scanf("%d",&x);
+	
+	
+	printf("\nenter y : ");
+	scanf("%d",&y);
+	
+	if(x>0 && y>0)
+	{
+		printf("Quadrant 1 (++)");
+	}
+	else if(x<0 && y>0)
+	{
+		printf("Quadrant 2 (-+)");
+	}
+	else if(x<0 && y<0)
+	{
+		printf("Quadrant 3 (--)");
+		
+	}
+	else
+	{
+		printf("Quadrant 4 (++)");
+	}
+}

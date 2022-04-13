a9e2c6580874abeec743eb7077bb30eddcce0610
fulendrabhagat <bhagatrahul984@gmail.com>
2022-04-13 15:19:27 +0530

    reverse of m digit no

---
+++ c programm/REVERSEDIGIT.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n,sum=0,r;
+	printf("Enter any number : ");
+	scanf("%d",&n);
+	while(n>0)    
+	{    
+	r=n%10;    
+	
+	sum=sum*10+r;
+	
+	n=n/10;    
+	}
+	printf("%d",sum);
+	
+	
+}
+

b827c453c5d6f3c0b2a46ba36c582c3baff1dec4
fulendrabhagat <bhagatrahul984@gmail.com>
2022-04-22 14:27:00 +0530

    pattern abc

---
+++ c programm/pattern ABC.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char r,c;
+	for(r=68;r>=65;r--)
+	{
+		for(c=65;c<=r;c++)
+		{
+			if(r%2==0)
+			printf("%c",c);
+			else
+			printf("%c",c+32);
+		}
+		printf("\n");
+	}
+}
+
---
+++ c programm/pattern ABCD.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char r,c;
+	for(r='A';r<='D';r++)
+	{
+		for(c='A';c<='D';c++)
+		{
+		printf("%c",c);
+		}
+		printf("\n");
+	}
+}

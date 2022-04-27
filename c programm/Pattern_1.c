cebe30022427e71fe94217782b080131fef7afc5
fulendrabhagat <bhagatrahul984@gmail.com>
2022-04-27 11:33:04 +0530

    Pattern 1

---
+++ c programm/Pattern_1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int r,c,a=1;
+	for(r=1;r<=5;r++)
+	{
+		for(c=1;c<=r;c++)
+		{
+			printf(" %d ",a++);
+			
+		}
+		          printf("\n");
+	}
+}
+

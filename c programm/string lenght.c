d4d834e819fdfa700ec6b3c69d9e3e546230eeb5
fulendrabhagat <bhagatrahul984@gmail.com>
2022-04-27 11:02:53 +0530

    string lenght

---
+++ c programm/string lenght.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+	char name[40];
+	printf("ENTER THE STRING\n");
+	scanf("%d",&name);
+	
+	printf("\n name lenght is %d",strlen(name));
+}

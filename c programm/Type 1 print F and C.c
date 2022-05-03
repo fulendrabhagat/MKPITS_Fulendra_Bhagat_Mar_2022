80b920440753481277672345f024d8f2bb741a7a
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-03 22:19:36 +0530

    function type 1 printf f and c

---
+++ c programm/Type 1 print F and C.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void func();
+int main()
+{
+	func();
+	
+}
+void func()
+{
+	printf("   ######\n");
+	printf("   #\n");
+	printf("   #\n");
+	printf("   #####\n");
+	printf("   #\n");
+	printf("   #\n");
+	printf("   #\n\n\n");
+	
+	
+	printf("    ######\n");
+	printf("  ##      ##\n");
+	printf(" #\n");
+	printf(" #\n");
+	printf(" #\n");
+	printf(" #\n");
+	printf(" #\n");
+	printf("  ##      ##\n");
+	printf("    ######\n");
+
+}

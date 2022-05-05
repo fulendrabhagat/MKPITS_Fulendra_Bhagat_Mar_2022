7b6555888af4f87d3c41c27363520b377f6fdd82
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-05 15:26:32 +0530

    diffrence min and maximun

---
+++ c programm/As 54 max and min diffrence.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	float a,b,c,d,dif;
+	float min,max;
+	printf("Enter Any Four Real Number:\n");
+	scanf("%f %f %f %f",&a,&b,&c,&d);
+
+	if(a>=b && a>=c && a>=d)
+	{
+		max=a;
+	}
+	else if(b>=a && b>=c && b>=d)
+	{
+		max=b;
+	}
+	else if(c>=a && c>=b && c>=d)
+	{
+		max=c;
+	}
+	else
+	{
+		max=d;
+	}
+
+	if(a<=b && a<=c && a<=d)
+	{
+		min=a;
+	}
+	else if(b<=a && b<=c && b<=d)
+	{
+		min=b;
+	}
+	else if(c<=a && c<=b && c<=d)
+	{
+		min=c;
+	}
+	else
+	{
+		min=d;
+	}
+
+	printf("Difference of minimum and maximum = .2f",max-min);
+}

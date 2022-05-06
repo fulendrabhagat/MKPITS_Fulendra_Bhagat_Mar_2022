302e4d46b576041b9698c630fe06332a9e5811ce
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-06 13:55:03 +0530

    multiplication of three number

---
+++ c programm/As 73 multi of three number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,c,d;
+	printf("enter three number for multiplication :> ");
+	scanf("%d %d %d",&a,&b,&c);
+	d=a*b*c;
+	printf("multiplication of three number is %d",d);
+}
+

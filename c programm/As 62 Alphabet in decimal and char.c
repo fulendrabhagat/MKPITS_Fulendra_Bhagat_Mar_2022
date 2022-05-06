98845ee79ef202957646d7ddf93fd238bb41cfd6
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-06 13:22:16 +0530

    alphabet in decimal

---
+++ c programm/As 62 Alphabet in decimal and char.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main() 
+{
+  char c;
+  printf("Capital Letter In Decimal : \n");
+  for (c=65;c<=90;c++) 
+  {
+    printf(" %c = %d \n ",c,c);
+  }
+
+  printf("Small Letters In Decimal : \n");
+  for (c=97;c<=122;c++) 
+  {
+    printf(" %c = %d \n ",c,c);
+  }
+
+}
+

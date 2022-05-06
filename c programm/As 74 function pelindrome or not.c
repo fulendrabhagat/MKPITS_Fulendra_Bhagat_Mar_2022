b73d1bdbf495d8fd58f95863b31e936aaa8bb8dd
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-06 13:59:53 +0530

    pelindrome function ot=r not

---
+++ c programm/As 74 function pelindrome or not.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n,rev=0,rem,orignal;
+	printf("enter number :>");
+	scanf("%d",&n);
+	orignal=n;
+	while(n)
+	{
+		rem=n%10;
+		rev=rev*10+rem;
+		n/=10;
+	}
+	if(rev==orignal)
+	{
+		printf("%d is palindrom",orignal);
+	}
+	else
+	{
+		printf("%d is not palindrom",orignal);
+	}
+}

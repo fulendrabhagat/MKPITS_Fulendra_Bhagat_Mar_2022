6ad3527288fa766434a480e0d0e458eb42d5a800
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-06 14:43:03 +0530

    triple sum

---
+++ c programm/As 75 triple sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,c;
+	
+	printf("enter 2 integer :? ");
+	scanf("%d %d",&a,&b);
+	c=a+b;
+	printf("sum is %d ",c);
+	if(a==b)
+	{
+		printf("triple of their of sum is %d",a+a+a);
+	}
+	
+}

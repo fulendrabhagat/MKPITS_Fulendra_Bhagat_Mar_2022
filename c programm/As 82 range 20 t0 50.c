e5e847ea649eb7eac09c92e665936456d0c75a27
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-06 20:32:23 +0530

    range 20 to 50

---
+++ c programm/As 82 range 20 t0 50.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n1,n2,n3;
+	
+	printf(" Enter any Three Number : ");
+	scanf("%d %d %d",&n1,&n2,&n3);
+
+	if(n1>=20&&n1<=50 || n2>=20&&n2<=50 || n3>=20&&n3<=50)
+	{
+		printf(" TRUE ");
+	} 
+	else
+	{
+		printf(" FALSE");
+	}
+}
+

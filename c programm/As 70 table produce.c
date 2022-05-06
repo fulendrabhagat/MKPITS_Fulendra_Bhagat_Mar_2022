9d890d0a45cc3879c136dc869ff27a7eb0f0bf8c
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-06 13:47:16 +0530

    produce a table

---
+++ c programm/As 70 table produce.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int x;
+    printf("x\tx+2\tx+4\tx+6\n\n");
+    for(x=1; x<=15; x+=3)
+    {
+       printf("%d\t%d\t%d\t%d\n",x,(x+2),(x+4),(x+6));  
+	}  
+}
+

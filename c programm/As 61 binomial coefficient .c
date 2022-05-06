81660115d62914362958a3b6db631f94e49fdc4a
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-06 13:18:36 +0530

    binomial coefficient table

---
+++ c programm/As 61 binomial coefficient .c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n, a, b;
+	n=0;
+	do
+	{
+		a = 0, b = 1;    
+		printf("%2d", n);
+	while(a<=n)
+	{
+		if(n==0 || a==0)
+		printf("%4d", b);
+	else
+	{
+		b=b* (n-a+1)/a;
+		printf("%4d", b);
+	}
+		a=a+1;
+	}
+		printf("\n");
+		n=n+1;
+	}
+	while(n<=10);
+
+
+
+}
+

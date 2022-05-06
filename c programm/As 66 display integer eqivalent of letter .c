9e109cab84d73c7639b73f3450c08f62ee878d85
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-06 13:34:50 +0530

    eqivalent of letters

---
+++ c programm/As 66 display integer eqivalent of letter .c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+  char c;
+  printf(" [a-z]: \n");
+  for (c=97;c<=122;c++) 
+  {
+    printf(" %d",c);
+  }
+  printf("\n\n [A-Z]: \n");
+  for (c=65;c<=90;c++) 
+  {
+    printf(" %d",c);
+  }
+
+
+}
+

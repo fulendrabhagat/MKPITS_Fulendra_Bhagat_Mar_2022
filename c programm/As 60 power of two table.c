c884571286df210c2e706ef026d902643a9e340b
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-06 13:14:59 +0530

    pwr of 2 table

---
+++ c programm/As 60 power of two table.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main() 
+{
+    int p,n;
+    float q;
+    p=1;
+    for(n=0; n<11; ++n) 
+    {
+      if (n == 0)
+      {
+        p = 1;
+      }
+      else
+      {
+        p = p * 2;
+        q = 1.0 /p;
+      }
+      printf("\n %d   %d  %f", n, p, q);
+    }
+
+  }

8c1cc2e6330cb845b9f8e2909580af647a90b569
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-06 13:10:22 +0530

    power of x

---
+++ c programm/As59 X power.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int x,n,i,power=1;
+	printf("Enter X & n : ");
+	scanf("%d %d",&x,&n);
+
+	for(i=1;i<=n;i++)
+	{
+		power=power*x;
+	}
+	printf("Power of %d is %d",x,power);
+}
+

3bbd177c6cfedf7fd0d76b94ee042c86bdae5a37
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-08 17:03:04 +0530

    check no of 5 s present in array

---
+++ c programm/As 89 Check No Of 5S present in Array.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,a[5],t=0;
+	printf("Enter Any five Number :\n");
+
+	for(i=0;i<5;i++)
+	{
+		printf("Enter number :");
+		scanf("%d",&a[i]);
+
+	}
+	int count = 0;
+    for (i = 0; i <5; i++)
+    {
+        if (a[i] == 5 && (a[i + 1] == 5 || a[i + 1] == 6)) 
+		{
+		count=count+1;
+		}
+    }
+
+    printf("%d", count);
+
+}
+

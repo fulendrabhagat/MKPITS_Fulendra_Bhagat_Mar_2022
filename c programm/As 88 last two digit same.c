b109cd79fc561eb05499e1e74334e76d12d3b3b7
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-08 16:55:33 +0530

    last two digit of number same or diffrent

---
+++ c programm/As 88 last two digit same.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n1,n2;
+	printf("Enter 1st Number :> ");
+	scanf("%d",&n1);
+	printf("Enter 2nd Number :> ");
+	scanf("%d",&n2);
+
+	if(n1%10==n2%10)
+	{
+		printf("\n Same last digit ");
+	}
+	else
+	{
+		printf("\n Last digit not name ");
+	}
+
+}
+

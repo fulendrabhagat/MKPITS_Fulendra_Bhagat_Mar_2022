a271d8c81fc803f1f8f73696dd7f46965559d322
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-08 17:04:31 +0530

    check triple present in array or not

---
+++ c programm/As 90 check triple present in array or not.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,a[5],t=0;
+	printf("Enter any five Number :\n");
+
+	for(i=0;i<5;i++)
+	{
+		printf("Enter number :> ");
+		scanf("%d",&a[i]);
+
+	}
+	for(i=0;i<5;i++)
+	{
+		int k=a[i];
+
+		if(a[i]==k && a[i+1]==k && a[i+2]==k)
+		{
+			printf("Triple");
+			t=1;
+		}
+	}
+	if(t==0)
+	{
+		printf("Not Triple");
+	}
+
+
+
+}
+

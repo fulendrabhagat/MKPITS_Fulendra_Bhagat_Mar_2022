e5f3b13c044ad42a811e3a6ed59cdb47ce587bdd
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-13 22:14:33 +0530

    greater integer

---
+++ c programm/As 97 greater intiger x y z.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int x,y,z;
+	printf("Enter value of x ");
+	scanf("%d",&x);
+	printf("Enter value of y ");
+	scanf("%d",&y);
+	printf("Enter value of z ");
+	scanf("%d",&z);
+	
+	if(y>x)
+	{
+		printf("Y is Greater Than X");
+	}
+	if(z>y)
+	{
+		printf("\nZ is Greater Than Y");
+	}
+	
+}
+

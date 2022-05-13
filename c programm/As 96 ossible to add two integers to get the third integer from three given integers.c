6d01ab96283c292fcea036c42663e0af52e6084c
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-13 22:13:22 +0530

    possible to add two int or get the 3rd int for 3rd int

---
+++ c programm/As 96 ossible to add two integers to get the third integer from three given integers.c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n1,n2,n3;
+	printf("Enter 1st Number ");
+	scanf("%d",&n1);
+	printf("Enter 2nd Number ");
+	scanf("%d",&n2);
+	printf("Enter 3rd Number ");
+	scanf("%d",&n3);
+	
+	if(n1==n2+n3 || n2==n1+n3 || n3==n1+n2)
+	{
+		printf("True");
+	}
+	else
+	{
+		printf("False");
+	}
+}
+

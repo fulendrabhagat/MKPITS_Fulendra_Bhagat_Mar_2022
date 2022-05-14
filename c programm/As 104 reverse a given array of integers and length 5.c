027f2519bc61985803124e69e3a05f9896ba9790
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-14 18:52:31 +0530

    reverse a given array of integerand lenght

---
+++ c programm/As 104 reverse a given array of integers and length 5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,j,a[5];
+	
+	for(i=0;i<5;i++)
+	{
+		printf("Enter Array Element : ");
+		scanf("%d",&a[i]);	
+	}
+	for(i=4;i>=0;i--)
+	{
+		printf("%d ",a[i]);
+	}
+}
+

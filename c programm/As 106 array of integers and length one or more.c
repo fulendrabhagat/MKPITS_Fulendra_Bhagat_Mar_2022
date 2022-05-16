932e63be6f13bc9682a7ffc7b54ad9db7d3d757c
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-16 09:06:28 +0530

    Array Of Integer and Length One Or More

---
+++ c programm/As 106 array of integers and length one or more.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,a[5],b[5],c[2];
+	
+	for(i=0;i<5;i++)
+	{
+		printf("Enter  Array element : ");
+		scanf("%d",&a[i]);
+	}
+	
+	printf("\n");
+	
+	c[0]=a[0];
+	c[1]=a[4];
+	for(int j=0;j<2;j++)
+	{
+		printf("%d ",c[j]);
+	}
+	
+}
+

f4558efea119df4ee7edbfe44406a24771c531ce
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-16 09:32:13 +0530

    multi,div,sub,sum

---
+++ c programm/As 116 algortithm.c
@@ -0,0 +1,20 @@
+/*Write a C program to print on screen the output of adding,subtracting,multiplying
+and dividing of two numbers which will be entered by the user.*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,sum,sub,mul,div;
+	printf("Enter the 1st number : ");
+	scanf("%d",&a);
+	printf("Enter the 2nd number : ");
+	scanf("%d",&b);	
+	sum=a+b;
+	sub=a-b;
+	mul=a*b;
+	div=a/b;
+	printf("Addition = %d\n",sum);
+	printf("Substraction = %d\n",sub);
+	printf("Multiplication = %d\n",mul);
+	printf("Division = %d",div);
+}

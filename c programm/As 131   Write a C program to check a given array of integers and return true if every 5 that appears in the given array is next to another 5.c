05dbb85a96a3fb18bc1cbc562140b27d726c82cc
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-19 13:09:55 +0530

    Return ture if every 5 thatappear in the array is next to another 5

---
+++ c programm/As 131   Write a C program to check a given array of integers and return true if every 5 that appears in the given array is next to another 5.c
@@ -0,0 +1,45 @@
+//Write a C program to check a given array of integers and return true if every 5 that appears in the given array is next to another 5.c
+
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,i,flag=0;
+    printf("Enter the size of array : ");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter the array element : ");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for (i = 0; i < n; i++)
+    {
+        if (a[i] == 5)
+        {
+            if ((i > 0 && a[i - 1] == 5) || (i < n - 1 && a[i + 1] == 5)) 
+            {
+                flag= 1;
+            }
+            else if (i == n - 1) 
+            {
+                flag = 0;
+            }
+            else 
+            {
+                flag=0;
+            }
+        }
+    }
+    
+    if(flag==1)
+    {
+        printf("TRUE");
+    }
+    else
+    {
+        printf("FALSE");
+    }
+}
+

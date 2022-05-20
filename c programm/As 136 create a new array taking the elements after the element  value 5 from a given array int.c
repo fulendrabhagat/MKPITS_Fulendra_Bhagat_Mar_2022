4903b49708126504f7182051b6c5b632510e5ee1
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-20 14:40:18 +0530

    New array taking element after the element value 5 from given array

---
+++ c programm/As 136 create a new array taking the elements after the element  value 5 from a given array int.c
@@ -0,0 +1,28 @@
+/*Write a C program to create a new array taking the elements after the element 
+value 5 from a given array of integers.*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i,j;
+    printf("Enter the size of array : ");
+    scanf("%d",&n);
+    int a[n],b[n];
+    printf("Enter the array element : \n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("New Array is : \n");
+    for (i=0;i<n;i++)
+    {
+       if(a[i]==5)
+    	{
+    	    for(j=i+1;j<n;j++)
+    	    {
+    	        printf("%d \n",a[j]);
+    	    }
+    	    break;
+    	}
+    }
+}

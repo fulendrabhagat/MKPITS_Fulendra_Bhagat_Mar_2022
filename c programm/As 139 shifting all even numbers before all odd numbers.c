c23a71ad44a6d8fbb5e520bd8f752a18a04828b6
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-20 15:05:08 +0530

    Shifting All Even Number Before All Odd Number

---
+++ c programm/As 139 shifting all even numbers before all odd numbers.c
@@ -0,0 +1,32 @@
+/*Write a C program to create new array from a given array of integers shifting 
+all even numbers before all odd numbers.*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i,t,l=0;
+    printf("Enter the size of array : ");
+    scanf("%d",&n);
+    int a[n],b[n];
+    printf("Enter the array element : \n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for (i = 0; i < n; i++)
+    {
+        if (a[i] % 2 == 0)
+        {
+            t = a[l];
+            a[l] = a[i];
+            a[i] = t;
+            l++;
+        }
+    }  
+    printf("New Array is : \n");
+    for (i=0;i<n;i++)
+    {
+    	printf("%d  \n",a[i]);
+	}
+    
+}

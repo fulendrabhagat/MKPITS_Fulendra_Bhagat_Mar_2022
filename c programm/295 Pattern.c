2770473fa192fa65b08dd577b2d69ab6c8f9ff44
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-20 15:19:04 +0530

    EEEEE DDDD CCC BB A pattern

---
+++ c programm/295 Pattern.c
@@ -0,0 +1,14 @@
+#include<stdio.h> 
+#include<conio.h>
+void main()
+{
+    int i, j;
+    for(i=5;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%c",'A'-1 + i);
+        }
+        printf("\n");
+    }
+}

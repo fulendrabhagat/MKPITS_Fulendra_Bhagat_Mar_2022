fb1331e95a2ac576ee5f0249d468450e44b7ad9d
fulendrabhagat <bhagatrahul984@gmail.com>
2022-05-20 15:42:47 +0530

    1 12 123

---
+++ c programm/300 Pattern.c
@@ -0,0 +1,22 @@
+#include <stdio.h>  
+#include<conio.h>  
+void main()  
+{  
+    int a=1;  
+    int i,j,k;
+    
+    for(i=3;i>=1;i--)  
+    {  
+        for(j=1;j<=i-1;j++)  
+        {  
+          printf(" ");  
+        }  
+        for( k=1;k<=a;k++)  
+        {  
+            printf("%d",k);  
+        }  
+        printf("\n");  
+        a++;  
+    }  
+}
+
